034c654a0c42f962d1b3a74fc202a963a5d8eab3
Vikram Singh <101462288+vikrams788@users.noreply.github.com>
2024-04-18 00:03:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const messageRoutes = require('./routes/messageRoutes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://srmu-connect-mern-self.vercel.app",
+    origin: "https://srmu-connect-mern-kappa.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
@@ -45,7 +45,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    orgin: 'https://srmu-connect-mern-self.vercel.app'
+    orgin: 'https://srmu-connect-mern-kappa.vercel.app'
   }
 })
 
