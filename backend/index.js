42af9b39e59492b0185d4d6c1b587a9dc38543c2
unknown <vikrams788058@gmail.com>
2024-05-03 22:40:16 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const teacherProfileRoutes = require('./routes/teacherProfileRoutes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://srmu-connect-mern-esjg.vercel.app",
+    origin: "https://srmu-connect-mern-alpha.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
@@ -47,7 +47,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    orgin: 'https://srmu-connect-mern-esjg.vercel.app'
+    orgin: 'https://srmu-connect-mern-alpha.vercel.app'
   }
 })
 
